d8b1274ee57687cd4a26e0c2123169c4358cc3bd
manish__24100 <166864864+manishkumar13953@users.noreply.github.com>
2026-02-27 15:16:30 +0530

    Update main.cpp
    name

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,15 @@
 #include <Arduino.h>
+/**
+ * @file main.ino
+ * @brief Embedded Temperature and Humidity Monitoring using DHT11
+ * @author MANISH_KUMAR
+ * @date 2026-02-09
+ *
+ * @details
+ * This program reads environmental data from the DHT11 sensor
+ * and displays temperature and humidity values on Serial Monitor.
+ * Students must complete the TODO sections.
+ */
 void setup() {
 // write your initialization code here
 }
